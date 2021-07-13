996c6a6d8a6a91953f8110415acf516b14631481
Megan V <elbowgrrl@gmail.com>
2021-07-13 14:13:00 -0700

    refactored with useApplicationData custom hook

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,5 +1,4 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
+import React from "react";
 import "components/Application.scss";
 import DayList from "components/DayList";
 import "components/Appointment";
@@ -9,81 +8,21 @@ import {
   getInterview,
   getInterviewersForDay,
 } from "../helpers/selectors";
+import useApplicationData from "hooks/useApplicationData";
 
 export default function Application(props) {
-  //set initial state for app
-
-  // const {
-  //   state,
-  //   setDay,
-
-  // }
-
-  //move state function to custom hook
-  const [state, setState] = useState({
-    day: "Monday",
-    days: [],
-    appointments: {},
-    interviewers: {},
-  });
-  // console.log("on render current state", state);
+  
+  const {
+    state,
+    setDay,
+    deleteInterview,
+    bookInterview
+  } = useApplicationData();
 
   const dailyAppointments = getAppointmentsForDay(state, state.day);
   const interviewers = getInterviewersForDay(state, state.day);
-
-  //move to custom hook
-  const setDay = (day) => setState({ ...state, day });
-
-  //move to custom hook
-  //make api requests to use data ffrom endpoints for state
-  useEffect(() => {
-    Promise.all([
-      axios.get(`/api/days`),
-      axios.get(`/api/appointments`),
-      axios.get(`/api/interviewers`),
-    ]).then((all) => {
-      const [days, appointments, interviewers] = all;
-
-      setState((prev) => ({
-        ...prev,
-        days: days.data,
-        appointments: appointments.data,
-        interviewers: interviewers.data,
-      }));
-    });
-  }, []);
-
-  //move to custom hook
-  function deleteInterview(id) {
-    //the id that gets passed to this function is a number
-    //id is an appmnt id in state at this level
-    let appointment = { ...state.appointments, [id]: null };
-    // console.log("in deleteInterview", appointment);
-
-    return axios.delete(`/api/appointments/${id}`, { appointment })
-    .then(() => {
-      setState({ ...state, appointment });
-    });
-  }
-
-  //move to custom hook
-  function bookInterview(id, interview) {
-    const appointment = {
-      ...state.appointments[id],
-      interview,
-    };
-    const appointments = {
-      ...state.appointments,
-      [id]: appointment,
-    };
-
-    return axios.put(`/api/appointments/${id}`, { interview }).then(() => {
-      setState({ ...state, appointments });
-    });
-  }
-
   const list = dailyAppointments.map((appointment) => {
-    const interview = getInterview(state, appointment.interview);
+  const interview = getInterview(state, appointment.interview);
 
     return (
       <Appointment
@@ -97,8 +36,6 @@ export default function Application(props) {
       />
     );
   });
-
-  // console.log("list", list);
 
   return (
     <main className="layout">
--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -1,8 +1,65 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
-const useApplicationData = function() {
+const useApplicationData = function () {
 
+  const [state, setState] = useState({
+    day: "Monday",
+    days: [],
+    appointments: {},
+    interviewers: {},
+  });
+  // console.log("on render current state", state);
 
+  const setDay = (day) => setState({ ...state, day });
+
+  //make api requests to use data ffrom endpoints for state
+  useEffect(() => {
+    Promise.all([
+      axios.get(`/api/days`),
+      axios.get(`/api/appointments`),
+      axios.get(`/api/interviewers`),
+    ]).then((all) => {
+      const [days, appointments, interviewers] = all;
+
+      setState((prev) => ({
+        ...prev,
+        days: days.data,
+        appointments: appointments.data,
+        interviewers: interviewers.data,
+        }));
+      });
+  }, []);
+
+  function deleteInterview(id) {
+    //the id that gets passed to this function is a number
+    //id is an appmnt id in state at this level
+    let appointment = { ...state.appointments, [id]: null };
+
+    return axios.delete(`/api/appointments/${id}`, { appointment })
+      .then(() => {
+        setState({ ...state, appointment });
+      });
+  }
+
+  function bookInterview(id, interview) {
+    const appointment = {
+      ...state.appointments[id],
+      interview,
+    };
+    const appointments = {
+      ...state.appointments,
+      [id]: appointment,
+    };
+
+    return axios.put(`/api/appointments/${id}`, { interview }).then(() => {
+      setState({ ...state, appointments });
+    });
+  }
+
+  return { state, setDay, deleteInterview, bookInterview };
 };
 
 export default useApplicationData;
+
+
